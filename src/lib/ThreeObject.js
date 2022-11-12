ec957a04bebda7bb8f34db8535067eeadb8cee56
tristan <heytristaann@gmail.com>
2022-11-11 21:54:12 -0500

    first commit

---
+++ src/lib/ThreeObject.js
@@ -0,0 +1,120 @@
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+
+// Draco Loader for Blender Models
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+dracoLoader.setDecoderConfig({ type: 'js' })
+
+// Loading Manager
+const loader = new GLTFLoader(new THREE.LoadingManager())
+loader.setDRACOLoader(dracoLoader)
+
+
+
+// Establish a new scene
+const SCENE = new THREE.Scene()
+
+// Load the statue model
+let ApolloModel;
+loader.load('./apollo.glb', (model) => {
+    model.scene.traverse((obj) => 
+        obj.material = new THREE.MeshPhongMaterial({ shininess: 45 })
+    );
+	ApolloModel = model.scene;
+    SCENE.add(ApolloModel);
+})
+
+
+
+// Camera
+const CAMERA = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 100)
+CAMERA.position.set(0, 1.8, 1.9)
+SCENE.add(CAMERA)
+
+// Left Side Sun Light (Gives that little glow)
+const SUN_LIGHT = new THREE.DirectionalLight(0x435c72, 0.08)
+SUN_LIGHT.position.set(-100, 0, -100)
+SCENE.add(SUN_LIGHT)
+
+// The light that moves with the cursor
+const MOVING_LIGHT = new THREE.PointLight(0x88b2d9, 0.4, 3, 3)
+MOVING_LIGHT.position.set(30, 3, 1.8)
+SCENE.add(MOVING_LIGHT)
+
+
+
+// Establish a global renderer variable
+let Renderer;
+
+// Animation function
+const animate = async () => {
+    Renderer.render(SCENE, CAMERA);
+    requestAnimationFrame(animate);
+}
+
+// The resize() function is used to resize the scene.
+// This is required for if the user resizes the site,
+// which is caught using the Window Resize Listener
+export const resize = async () => {
+	// Set the pixel ratio
+	Renderer.setPixelRatio(window.devicePixelRatio);
+	// Set the screen size
+	Renderer.setSize(window.innerWidth, window.innerHeight);
+	// Set the camera aspect ratio (most likely 16:9)
+	CAMERA.aspect = window.innerWidth / window.innerHeight;
+	// Update projection matrix
+	CAMERA.updateProjectionMatrix();
+};
+// Window Resize Listener
+window.addEventListener('resize', resize);
+
+// The setScene() function is the primary function
+// for updating the sphere's scene data.
+export const setScene = async (canvas) => {
+	Renderer = new THREE.WebGLRenderer({
+		powerPreference: "high-performance",
+		antialias: true,
+		canvas: canvas,
+		alpha: true
+	});
+	Renderer.autoClear = true
+	Renderer.setPixelRatio(window.devicePixelRatio, 1)
+	Renderer.setSize(window.innerWidth, window.innerHeight)
+	Renderer.outputEncoding = THREE.sRGBEncoding
+
+	// Size the scene
+	await resize();
+
+	// Animate the sphere
+	await animate();
+};
+
+// Store the previous elapsed time
+let PreviousElapsedTime = 0;
+
+// On mouse move, move the light source with it
+document.addEventListener('mousemove', (e) => {
+    // Establish Delta Time for smooth light movement
+    const ELAPSED_TIME = performance.now() / 1000;
+    const DELTA_TIME = ELAPSED_TIME - PreviousElapsedTime;
+    PreviousElapsedTime = ELAPSED_TIME;
+
+    // Establish X and Y constants
+    const Y = (e.clientY / window.innerHeight - 0.4) * 9;
+    const X = (e.clientX / window.innerWidth - 0.5) * 8;
+
+    // Move the light source
+    MOVING_LIGHT.position.y -= (Y + MOVING_LIGHT.position.y - 2) * DELTA_TIME;
+    MOVING_LIGHT.position.x += (X - MOVING_LIGHT.position.x) * 2 * DELTA_TIME;
+
+	// Move the statue position slightly
+	if (Math.abs(ApolloModel.position.z) > 0.1) {
+		ApolloModel.position.z = 0
+		ApolloModel.position.x = 0
+	} else {
+		ApolloModel.position.z -= ((Y * 0.015) + ApolloModel.position.z) * DELTA_TIME;
+    	ApolloModel.position.x += ((X * 0.015) - ApolloModel.position.x) * DELTA_TIME;
+	}
+}, false)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	// Library Imports
+	import { setScene } from '$lib/ThreeObject.js';
+	// import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+    
+	// Create a new wavy sphere scene
+	let ThreeObject;
+
+	// On site load
+    onMount(async () => {
+		// Set the threejs scene
+		await setScene(ThreeObject)
+
+        // Custom Cursor
+        const CUSTOM_CURSOR = document.querySelector('.cursor');
+        document.addEventListener('mousemove', (e) => 
+            CUSTOM_CURSOR.style.cssText =`left: ${e.clientX - 11.5}px; top: ${e.clientY - 13}px;`, false
+        );
+    });
+</script>
+
+<!-- Header -->
+<div id="wrapper" class="h-screen text-center flex items-center text-white justify-center flex-col relative z-10 pointer-events-none">
+	<h2 class="text-4xl font-thin tracking-[0.25em] italic m-9 point">Apollo</h2>
+	<h1 class="text-6xl font-thin m-3 tracking-[0.25em] mb-4">θεός των πάντων</h1>
+	<p class="text-lg font-thin m-3 tracking-[0.25em] w-[90rem]">Apollo is one of the Olympian deities in classical Greek and Roman religion and Greek and Roman mythology. The national divinity of the Greeks, Apollo has been recognized as a god of archery, music and dance, truth and prophecy, healing and diseases, the Sun and light, poetry, and more. One of the most important and complex of the Greek gods, he is the son of Zeus and Leto, and the twin brother of Artemis, goddess of the hunt. Seen as the most beautiful god and the ideal of the kouros (ephebe, or a beardless, athletic youth), Apollo is considered to be the most Greek of all the gods. Apollo is known in Greek-influenced Etruscan mythology as Apulu.</p>
+</div>
+
+<!-- Custom Cursor -->
+<div class="cursor fixed pointer-events-none border-2 border-white p-[0.6rem] rounded-full mix-blend-difference z-10 ease-linear"></div>
+
+<!-- The 3D Wave Sphere -->
+<canvas id="statue" bind:this={ThreeObject} class="pointer-events-none fixed top-0 left-0 outline-none z-0 h-[100vh] w-[100vw] bg-black"/>
+
+<!-- Styling -->
+<style>
+	#statue { background: radial-gradient(circle at center center, #171717 0, #050505 58%); }
+	#wrapper { background: linear-gradient(0deg, rgba(5, 5, 5, 1) 15%, rgba(39, 39, 39, 0) 80%); }
+</style>
